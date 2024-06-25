d267bdba69c81ff8f4a6b9eebd844ebbb3026640
ctapiadev <ctapiadev@gmail.com>
2024-06-25 01:37:58 -0400

    include class

---
+++ src/clase8/VariablesTiposDeDatos.java
@@ -0,0 +1,27 @@
+
+package clase8;
+
+public class VariablesTiposDeDatos {
+    
+    public static void main(String[] args) {
+        
+        byte nbyte = 120;
+        short nshort = 32000;
+        int num = 25405;
+        long nlong = 234403203;
+        float nfloat = 23.4f;
+        double ndouble = 23.4;
+        char simbol = 'A';
+        boolean logic = true;
+        
+        System.out.println(nbyte);
+        System.out.println(nshort);
+        System.out.println(num);
+        System.out.println(nlong);
+        System.out.println(nfloat);
+        System.out.println(ndouble);
+        System.out.println(simbol);
+        System.out.println(logic);
+    }
+    
+}
