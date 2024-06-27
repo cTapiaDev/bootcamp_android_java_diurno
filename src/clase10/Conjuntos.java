1f3e62ee28cf011b899bd6a26c97ddfcad7b4a35
ctapiadev <ctapiadev@gmail.com>
2024-06-27 10:15:59 -0400

    conjuntos y mapas

--- src/clase10/Conjuntos.java
+++ src/clase10/Conjuntos.java
@@ -1,5 +1,53 @@
 package clase10;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Conjuntos {
+    
+    public static void main(String[] args) {
+        
+        // Crear conjunto
+        Set<String> ciudades = new HashSet<>();
+        
+        // Agregar elementos
+        ciudades.add("Santiago");
+        ciudades.add("Rancagua");
+        ciudades.add("Temuco");
+        ciudades.add("Santiago");
+        
+        // Verificar si existe
+        System.out.println(ciudades.contains("Talca"));
+        
+        // Eliminar un elemento
+        ciudades.remove("Rancagua");
+        
+        // Recorrer el conjunto
+        for (String e : ciudades) {
+            System.out.println(e);
+        }
+        
+//        // Vaciar el conjunto
+        ciudades.clear();
+
+//        Set<Integer> conjunto1 = new HashSet<>();
+//        Set<Integer> conjunto2 = new HashSet<>();
+//        
+//        conjunto1.add(1);
+//        conjunto1.add(2);
+//        conjunto1.add(3);
+//        
+//        conjunto2.add(2);
+//        conjunto2.add(3);
+//        conjunto2.add(4);
+//        
+//        conjunto1.retainAll(conjunto2);
+//        
+//        for(Integer e: conjunto1) {
+//            System.out.println(e);
+//        }
+        
+        
+    }
     
 }
--- src/clase10/Mapas.java
+++ src/clase10/Mapas.java
@@ -1,5 +1,33 @@
 package clase10;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mapas {
     
+    public static void main(String[] args) {
+        
+        // Crear un mapa
+        Map<Integer, String> ciudades = new HashMap<>();
+        
+        // Agregar dato
+        ciudades.put(1, "Talca");
+        ciudades.put(2, "Santiago");
+        ciudades.put(3, "Linares");
+        
+        // Obtener un valor
+        System.out.println(ciudades.get(6));
+        
+        // Verificar existencia de clave
+        System.out.println(ciudades.containsKey(1));
+        
+        // Eliminar dato
+        ciudades.remove(3);
+        
+        // Recorrer un mapa
+        for (Map.Entry<Integer, String> e : ciudades.entrySet()) {
+            System.out.println(e);
+        }
+        
+    }
 }
