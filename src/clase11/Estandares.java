b759508bcf0a0fc0e6d11eb0cb3856b018f221c2
ctapiadev <ctapiadev@gmail.com>
2024-06-27 11:32:46 -0400

    estandares

---
+++ src/clase11/Estandares.java
@@ -0,0 +1,47 @@
+package clase11;
+
+public class Estandares {
+
+    public static void main(String[] args) {
+
+        int num = 0; // Esta variable es de tipo int
+        System.out.println(num); // Es para mostrar mi variable
+        
+        /*
+            Este es un bloque
+            de código
+            con más de una
+            línea
+        */
+        
+        
+        // Este es un comentario de una sola línea
+        /* Este es otro comentario de una sola línea */
+        
+        // Inicio Ciclo
+        // -------
+        // Fin Ciclo
+        
+        
+        String arreglo[] = new String[10];
+        
+//        while (true) {
+//            
+//        }
+
+        int suma = 10 + 20 + 2 + 4;
+        
+//        for (int i = 0 ; i < suma; i++) {
+//            
+//        }
+
+        String nombreUsuario = "Goku";
+        System.out.println(nombreUsuario);
+        
+        int LONGITUD_MAXIMA = 200;
+        
+        System.out.println(LONGITUD_MAXIMA);
+
+    }
+
+}
